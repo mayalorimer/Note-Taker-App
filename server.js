1723649f627fc92b87b417d1fc04fbc365cc9b58
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-26 12:04:23 -0700

    API routes changed

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const express = require('express'); 
+const path = require('path'); 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express(); 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 // get route should return notes.html
 app.get('/notes', (req, res) =>
@@ -23,10 +28,33 @@ app.get('/api/notes', (req, res) => {
             console.error(err);
         }
         else {
-            const savedNotes = JSON.parse(data); 
+            const notes = JSON.parse(data);
+            res.json(notes);
         }
-    })
-})
+    });
+});
+
+// API post route to recieve a new note and add it to the db.json
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'uft8', (err, data) => {
+        if (err) {
+            console.error(err); 
+        }
+        else {
+            const notes = JSON.parse(data);
+            const newNote = req.body;
+            notes.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                else {
+                    res.json(newNote); 
+                }
+            })
+        }
+    });
+});
 
 //initializes the port
 app.listen(PORT, () => {
